653fcc5b68fdb21618a437affb3181bff725ef2f
warchambault5 <tbm20rules@gmail.com>
2025-10-16 20:58:02 -0500

    added constants for pull

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,12 @@
+pub mod config {
+    /// Wheel-Wheel width of robot.
+    pub const WHEELBASE_WIDTH_INCHES: f64 = 10.0;
+    /// Wheel-Wheel length of robot.
+    pub const WHEELBASE_LENGTH_INCHES: f64 = 10.0;
+
+
+}
+
 pub mod vision {
     use nalgebra::Vector2;
 
