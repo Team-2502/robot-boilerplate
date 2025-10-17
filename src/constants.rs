c73a2d261a8f6373c840bcbb2bea2d366ced969a
warchambault5 <tbm20rules@gmail.com>
2025-10-16 21:14:44 -0500

    added vision template

--- src/constants.rs
+++ src/constants.rs
@@ -6,3 +6,38 @@ pub mod config {
 
 
 }
+
+pub mod vision {
+    use nalgebra::Vector2;
+
+    /// pitch of the limelight in degrees
+    pub const LIMELIGHT_PITCH_DEGREES: f64 = 0.;
+    /// the yaw of the limelight in degrees (counterclockwise positive)
+    pub const LIMELIGHT_YAW_DEGREES: f64 = 90.;
+    /// limelights height off the ground in inches
+    pub const LIMELIGHT_HEIGHT_INCHES: f64 = 20.92;
+
+    /// Distance from center of robot to limelight in inches as vector2 (x,y)
+    pub const ROBOT_CENTER_TO_LIMELIGHT_INCHES: Vector2<f64> = Vector2::new(11.118, 10.352);
+
+    /// # This compensates for underestimation caused by angled views of the target.
+    /// Calculated with the formula:
+    /// ```text
+    /// TX_FUDGE_FACTOR = (real_distance - estimated_distance) / (estimated_distance * |tx|)
+    /// ```
+    /// Increase distance by 13.5% for every 20 degrees of absolute value of tx
+    ///
+    /// Set this to 0 for new robots
+    pub const TX_FUDGE_FACTOR: f64 = 0.;
+}
+
+pub mod pose_estimation {
+    /// the starting fom for the limelight in meters
+    pub const LIMELIGHT_BASE_FOM: f64 = 0.001;
+    /// meters of inaccuracy per degree of tx absolute value (if limelight is miscalibrated)
+    pub const LIMELIGHT_INACCURACY_PER_DEGREE_TX: f64 = 0.015;
+    /// meters of inaccuracy per (radian/second) of drivetrain angular velocity
+    pub const LIMELIGHT_INACCURACY_PER_ANGULAR_VELOCITY: f64 = 2.;
+    ///  meters of inaccuracy per (meter/second) of drivetrain linear velocity
+    pub const LIMELIGHT_INACCURACY_PER_LINEAR_VELOCITY: f64 = 2.;
+}
