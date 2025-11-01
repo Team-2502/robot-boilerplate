e2d0c04b9203c9c73d57243c5b1aa3289544843a
warchambault5 <tbm20rules@gmail.com>
2025-11-01 14:28:28 -0500

    deleted other optimize in wrong spot (i sold)

--- src/swerve/kinematics.rs
+++ src/swerve/kinematics.rs
@@ -101,31 +101,6 @@ impl Kinematics {
         targets = self.scale_targets(targets);
         targets
     }
-
-    pub fn optimize(speed: f64, current_angle: Angle, target_angle: Angle) -> (f64, Angle) {
-        let angle_rad = current_angle.get::<radian>();
-        let target_rad = target_angle.get::<radian>();
-
-        let mut difference = (angle_rad - target_rad + PI) % (2.0 * PI) - PI;
-        if difference < -PI {
-            difference += 2.0 * PI;
-        }
-
-        let mut optimized_speed = speed;
-        if difference.abs() > FRAC_PI_2 {
-            optimized_speed *= -1.0;
-
-            if difference > 0.0 {
-                difference = -PI + difference;
-            } else {
-                difference = PI + difference;
-            }
-        }
-
-        let optimized_angle = target_angle + Angle::new::<radian>(difference);
-        (optimized_speed, optimized_angle)
-    }
-
 }
 
 // run tests with
